250a3e2792636f151192d31821fe4edba43511cf
Emile Savoie <emile.savoie@ena.ca>
2024-10-21 14:26:37 -0400

    Working serial comm
    Issues with servo connection to the board

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,42 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-Servo servo1;
+#define PIN_SERVO1 5
+#define PIN_SERVO2 10
 
-int servoPin = 2;
+Servo servo1;
+Servo servo2;
 
 void setup()
 {
-  servo1.attach(servoPin);
+  Serial.begin(9600);
+  Serial.println("Arduino Ready");
+
+  servo1.attach(PIN_SERVO1);
+  servo2.attach(PIN_SERVO2);
+
+  // servo1.write(HOME_SERVO_1);
+  // servo2.write(HOME_SERVO_2);
+  servo1.write(0);
+  servo2.write(0);
 }
 
 void loop()
 {
-  servo1.write(0);
+
+  // RECEIVE DATA
+  if (Serial.available() > 0)
+  {
+    String receivedData = Serial.readStringUntil('\n');
+    Serial.println("msg received");
+
+    if (receivedData == "Landed")
+    {
+      Serial.println("Landed received");
+      servo1.write(100);
+      servo2.write(100);
+      Serial.println("servo moved");
+    }
+  }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
